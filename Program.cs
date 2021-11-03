ccbc98873d27fc54623a00aa3ad3ba97039f4bef
s20404 <80415193+s20404@users.noreply.github.com>
2021-11-03 18:52:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Monte_Carlo
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var random = new Random();
+            
+            const int r = 1;
+            var pointsIn = 0.0;
+            var allPoints = 0.0;
+
+            foreach (var unused in Enumerable.Range(0, 1000))
+            {
+                var newPointX = random.NextDouble();
+                var newPointY = random.NextDouble();
+                
+                pointsIn += PointIsInCircle(newPointX, newPointY, r) ? 1 : 0;
+                
+                allPoints += 1;
+            }
+
+            var estimation =  4 * pointsIn/allPoints;
+            
+            Console.WriteLine($"Estimation of pi: {estimation}");
+        }
+
+        private static bool PointIsInCircle(double x, double y, int r) =>
+            x*x + y*y < r;
+    }
+}
